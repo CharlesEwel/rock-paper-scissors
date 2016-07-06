d7947074007d9f7b46c282629f532587dfee7ecc
Shradha Pulla and Charlie Ewel <pullashradha@gmail.com>
2016-07-06 08:45:16 -0700

    Add conditional statements to RockPaperScissors.cs for all possible combinations of game

--- Objects/RockPaperScissors.cs
+++ Objects/RockPaperScissors.cs
@@ -8,13 +8,46 @@ namespace RockPaperScissors.Objects
   {
     public string Shoot (string input1, string input2)
     {
-      if(input1 == input2)
+      if (input1 == input2)
       {
         return "This round was a draw";
       }
+      else if (input1 == "rock")
+      {
+        if (input2 == "paper")
+        {
+          return "User2 wins";
+        }
+        else
+        {
+          return "User1 wins";
+        }
+      }
+      else if (input1 == "paper")
+      {
+        if (input2 == "rock")
+        {
+          return "User1 wins";
+        }
+        else
+        {
+          return "User2 wins";
+        }
+      }
+      else if (input1 == "scissors")
+      {
+        if (input2 == "rock")
+        {
+          return "User2 wins";
+        }
+        else
+        {
+          return "User1 wins";
+        }
+      }
       else
       {
-        return "This round was not a draw";
+        return "Invalid game";
       }
     }
   }
--- Tests/RockPaperScissorsTest.cs
+++ Tests/RockPaperScissorsTest.cs
@@ -7,7 +7,19 @@ namespace RockPaperScissors.Objects
     public void Shoot_SameInputs_Draw()
     {
       Game newGame = new Game ();
-      Assert.Equal("This round was a draw", newGame.Shoot("Rock","Rock"));
+      Assert.Equal("This round was a draw", newGame.Shoot("rock","rock"));
+    }
+    [Fact]
+    public void Shoot_RockScissors_Winner()
+    {
+      Game newGame = new Game ();
+      Assert.Equal("User1 wins", newGame.Shoot("rock","scissors"));
+    }
+    [Fact]
+    public void Shoot_RockPaper_Winner()
+    {
+      Game newGame = new Game ();
+      Assert.Equal("User2 wins", newGame.Shoot("rock","paper"));
     }
   }
 }
