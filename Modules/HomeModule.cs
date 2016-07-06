b1da1665cdbf6e9c755d0d5dafb8c54212e6cf5d
Shradha Pulla and Charlie Ewel <pullashradha@gmail.com>
2016-07-06 09:03:33 -0700

    Add html to index.cshtml, add code to HomeModule and the program runs without any errors!

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,7 +9,12 @@ namespace RockPaperScissors
   {
     public HomeModule()
     {
-
+      Get["/"] = _ => View["index.cshtml", "Prepare for Rock, Paper, Scissors"];
+      Post["/shoot"] = _ => {
+        Game newGame = new Game();
+        string result = newGame.Shoot(Request.Form["input1"], Request.Form["input2"]);
+        return View["index.cshtml", result];
+      };
     }
   }
 }
--- Objects/RockPaperScissors.cs
+++ Objects/RockPaperScissors.cs
@@ -12,42 +12,13 @@ namespace RockPaperScissors.Objects
       {
         return "This round was a draw";
       }
-      else if (input1 == "rock")
+      else if ((input1=="rock"&&input2=="scissors") || (input1=="scissors"&&input2=="paper") || (input1=="paper"&&input2=="rock"))
       {
-        if (input2 == "paper")
-        {
-          return "User2 wins";
-        }
-        else
-        {
-          return "User1 wins";
-        }
-      }
-      else if (input1 == "paper")
-      {
-        if (input2 == "rock")
-        {
-          return "User1 wins";
-        }
-        else
-        {
-          return "User2 wins";
-        }
-      }
-      else if (input1 == "scissors")
-      {
-        if (input2 == "rock")
-        {
-          return "User2 wins";
-        }
-        else
-        {
-          return "User1 wins";
-        }
+        return "User1 wins";
       }
       else
       {
-        return "Invalid game";
+        return "User2 wins";
       }
     }
   }
